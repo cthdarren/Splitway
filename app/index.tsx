1478b0275bb8f56f91ffed19056f5919a35b717b
cthdarren <ctze.darren@gmail.com>
2024-12-19 15:50:35 +0100

    started fresh slate, installed tailwind and nativewind

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "top",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-7xl">SplitWay</Text>
+    </View>
+  );
+}
