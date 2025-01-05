5543381432a3a73cec7abc583cb75303c09f8cf1
cthdarren <ctze.darren@gmail.com>
2025-01-05 21:19:49 +0100

    fixed vert alignment for avatar picture

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -7,12 +7,12 @@ import { ReactNode } from "react";
 function ScreenWrapper({ children }: { children: ReactNode }) {
     return (
         <View className="flex py-5 px-8 h-full">
-            <View className="flex flex-row p-0 justify-between">
+            <View className="flex flex-row p-0 justify-between items-center">
                 <Text className="text-3xl py-2">{strings.APP_NAME}</Text>
                 <TouchableHighlight
                     underlayColor={"rgba(0, 0, 0, 0.0)"}
                     onPress={() => { }}>
-                    <FontAwesome size={40} name="user-circle" />
+                    <FontAwesome size={36} name="user-circle" />
                 </TouchableHighlight>
             </View>
             {children}
