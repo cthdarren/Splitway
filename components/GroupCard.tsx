af8db1771bbb9b6d052b06c7a6da570d361e820c
cthdarren <ctze.darren@gmail.com>
2025-01-05 22:14:01 +0100

    changed groupcard style, added default image

--- components/GroupCard.tsx
+++ components/GroupCard.tsx
@@ -1,18 +1,25 @@
-import { Text, View, TouchableOpacity } from "react-native";
+import { Text, Image, View, TouchableOpacity } from "react-native";
 import { router } from "expo-router";
 import { GroupCardProps } from "@/types/componentproptypes";
 
 function GroupCard({ id, title, expenditure, currency }: GroupCardProps) {
-    return (
-        <TouchableOpacity onPress={()=> router.push(`/group/${id}`)}>
-            <View className="bg-gray-200 px-3 w-full rounded-md mt-8 py-6 flex flex-row justify-between">
-                <Text className="text-xl">{title}</Text>
-                <Text className="text-xl text-right">
-                    {currency + expenditure}
-                </Text>
-            </View>
-        </TouchableOpacity>  
-    );
+  return (
+    <TouchableOpacity onPress={() => router.push(`/group/${id}`)}>
+      <View className="bg-gray-200 w-full rounded-md mt-5 flex flex-row justify-between items-center">
+        <View className="rounded-full w-[60px] overflow-hidden mx-3">
+          <Image
+            source={require("@/assets/images/default_pic.png")}
+            style={{ height: 60, width: 60 }}
+            resizeMode="cover"
+          />
+        </View>
+        <View className="pr-4 flex py-7 justify-evenly flex-grow">
+          <Text className="text-xl font-bold pb-1">{title}</Text>
+          <Text className="text-xl ">{currency + expenditure}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 export default GroupCard;
